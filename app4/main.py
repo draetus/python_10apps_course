a8b4ffce85e71e701daefe633e0e2832f044954d
Draetus <johndoe@example.com>
2016-12-12 18:18:58 -0200

    Upgrading app4

--- app4/main.py
+++ app4/main.py
@@ -24,38 +24,46 @@ def run_event_loop():
         a - Adds an entry to the journal.
         x - Exit and save the journal.
     '''
-    print('What do you want to do with your journal?')
-    cmd = None
-    journal_name = 'default'
+    journal_name = input('Inform the name of the file <Enter to default>:\n')
+    if journal_name.strip() == '':
+        journal_name = 'default'
     journal_data = journal.load(journal_name)
+    recentdata = []
+    print('\nWhat do you want to do with your journal?')
+    cmd = None
 
     while cmd != 'x':
-        cmd = input('[L]ist entries \n [A]dd an entry \n E[x]it: \n')
+        cmd = input('\n[L]ist entries \n [A]dd an entry \n E[x]it: \n')
         cmd = cmd.lower().strip()
 
         if cmd == 'l':
-            list_entries(journal_data)
+            list_entries(journal_data,recentdata)
         elif cmd == 'a':
-            add_entry(journal_data)
+            add_entry(journal_data,recentdata)
         elif cmd != 'x' and cmd:
             print("Sorry, we don't understand '{}'.".format(cmd))
     print('Finished')
     journal.save(journal_name,journal_data)
 
 
-def list_entries(data):
+def list_entries(data,recentdata):
     '''
     This list the entries of the current journal
 
     :param data: The current journal data
     '''
-    print('This is your journal entries:')
+    print('\nThis is your entire journal entries:')
     entries = reversed(data)
     for index,entry in enumerate(entries):
         print('[{0}] {1}'.format(index+1,entry))
 
+    print('\nThis is your recent made journal entries:')
+    entries = reversed(recentdata)
+    for index,entry in enumerate(entries):
+        print('[{0}] {1}'.format(index+1,entry))
 
-def add_entry(data):
+
+def add_entry(data,recentdata):
     '''
     This adds an entry to the current journal
 
@@ -64,6 +72,7 @@ def add_entry(data):
     text = input('Type your entry, <enter> to exit: ')
     if text != '':
         data.append(text)
+        recentdata.append(text)
 
 if __name__ == '__main__':
     print_header()
