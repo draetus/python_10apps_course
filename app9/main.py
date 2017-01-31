21aa8c9e37b89d6e2ddd0ea63edb58b375625340
Draetus <johndoe@example.com>
2017-01-31 18:00:55 -0200

    Real Estate Analysis App

--- app9/main.py
+++ app9/main.py
@@ -1,4 +1,5 @@
 import os
+import statistics
 import csv
 import data_types
 
@@ -24,7 +25,7 @@ def get_data_file():
 
 
 def load_file(filename):
-    with open(filename, 'r', encoding='utf-8') as fin:
+    with open(filename,'r',encoding='utf-8') as fin:
         
         reader = csv.DictReader(fin)
         purchases = []
@@ -33,27 +34,51 @@ def load_file(filename):
             p = data_types.Purchase.create_from_dict(row)
             purchases.append(p)
 
-        print(purchases[0].__dict__)
+        return purchases
 
-
-#LOADING WITHOUT THE CSV MODULE
-#def load_file(filename):
-#    with open(filename,'r',encoding = 'utf-8') as fin:
-#        header = fin.readline().strip()
-#        print('Found header: ' + header)
-#
-#        lines = []
-#        for line in fin:
-#            line_data = line.strip().split(',')
-#            bed_count = line_data[4]
-#            lines.append(line_data)
-#
-#        print(lines[:5])
-
-
+def get_price(p):
+    return p.price
 
 def query_data(data):
-    pass
+    data.sort(key=lambda p: p.price)
+
+    high_purchase = data[-1]
+    print('The most expensive house is {:,}$ with {} beds and {} baths'.format(
+        high_purchase.price, high_purchase.beds,high_purchase.baths))
+
+    low_purchase = data[0]
+    print('The least expensive house is {:,}$ with {} beds and {} baths'.format(
+        low_purchase.price,low_purchase.beds,low_purchase.baths))
+
+    prices = [
+            p.price
+            for p in data]
+
+    ave_price = statistics.mean(prices)
+
+    print('The average home price is {:,}$'.format(int(ave_price)))
+
+    two_bed_homes = (
+            p
+            for p in data
+            if announce(p,'2-bedrooms, found {}'.format(p.beds)) and p.beds == 2)
+
+    homes = []
+    for h in two_bed_homes:
+        if len(homes) > 5:
+            break
+        homes.append(h)
+
+    ave_price = statistics.mean((announce(p.price, 'price') for p in homes))
+    ave_baths = statistics.mean((p.baths for p in homes))
+    ave_sqft  = statistics.mean((p.sqft for p in homes))
+    print('The average 2 bedroom home is {:,}$, baths={}, sq ft={:,}'.format(
+        int(ave_price), round(ave_baths,1), round(ave_sqft,1)))
+
+
+def announce(item,msg):
+    print('Pulling item {} for {}'.format(item,msg))
+    return item
 
 
 if __name__ == '__main__':
